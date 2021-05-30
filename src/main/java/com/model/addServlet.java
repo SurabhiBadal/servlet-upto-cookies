788a5819fb8286194e8bc1d5ebea733128e862cf
SurabhiBadal <s.badal2012@gmail.com>
2021-05-31 00:12:31 +0530

    Handling only post request change servive method to doPost

--- src/main/java/com/model/addServlet.java
+++ src/main/java/com/model/addServlet.java
@@ -3,24 +3,26 @@ package com.model;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class addServlet extends HttpServlet{
 	
-	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException {
 		int i=Integer.parseInt(req.getParameter("num1"));
 		int j=Integer.parseInt(req.getParameter("num2"));
 		int k=i+j;
 		// getting response on console
 		//System.out.println("The sum of the numbers="+k);
 		
-		//we want response on our webpage to do so we use PrintWriter
+		//we want response on our client page to do so we use PrintWriter
 		
 		PrintWriter out=res.getWriter();
 		out.println("The result is"+k);
 		
 	}
+	
 
 }
